5deb4f76becae44ccaaa97a8f9fee864125a36c1
NoraSharaby <47696706+NoraSharaby@users.noreply.github.com>
2019-02-24 21:00:03 +0200

    Add files via upload

--- ML_Assignment1.m
+++ ML_Assignment1.m
@@ -99,27 +99,29 @@ X_test=[ones(m_test,1) U_test U1_test U_test.^2 U_test.^3];  % Adding different 
 % X=[ones(m,1) U U1 U.^2 U.^3 U.^4];
 
 Y_test=T{m_train+1:m_train+m_test,3}/mean(T{m_train+1:m_train+m_test,3});         % Price column
-Theta_test=zeros(n,1);
-k_test=1;
+E_test=(1/(2*m_test))*sum((X_test*Theta_train-Y_test).^2);
 
-E_test(k_test)=(1/(2*m_test))*sum((X_test*Theta_test-Y_test).^2);
+% Theta_test=zeros(n,1);
+% k_test=1;
+% 
+% E_test(k_test)=(1/(2*m_test))*sum((X_test*Theta_test-Y_test).^2);
+% 
+% R_test=1;
+% while R_test==1                     % Gradient decent
+% Alpha=Alpha*1;
+% Theta_test=Theta_test-(Alpha/m_test)*X_test'*(X_test*Theta_test-Y_test);
+% k_test=k_test+1;
+% E_test(k_test)=(1/(2*m_test))*sum((X_test*Theta_test-Y_test).^2);
+% if E_test(k_test-1)-E_test(k_test)<0
+%     break
+% end 
+% q=(E_test(k_test-1)-E_test(k_test))./E_test(k_test-1);
+% if q <.000001;
+%     R_test=0;
+% end
+% end
 
-R_test=1;
-while R_test==1                     % Gradient decent
-Alpha=Alpha*1;
-Theta_test=Theta_test-(Alpha/m_test)*X_test'*(X_test*Theta_test-Y_test);
-k_test=k_test+1;
-E_test(k_test)=(1/(2*m_test))*sum((X_test*Theta_test-Y_test).^2);
-if E_test(k_test-1)-E_test(k_test)<0
-    break
-end 
-q=(E_test(k_test-1)-E_test(k_test))./E_test(k_test-1);
-if q <.000001;
-    R_test=0;
-end
-end
-
-%test...................................
+%CV...................................
 m_cv=(m-m_train)/2;
 
 
@@ -135,24 +137,25 @@ X_cv=[ones(m_cv,1) U_cv U1_cv U_cv.^2 U_cv.^3];  % Adding different Us or changi
 % X=[ones(m,1) U U1 U.^2 U.^3 U.^4];
 
 Y_cv=T{m_train+m_test+1:m_train+m_test+m_cv,3}/mean(T{m_train+m_test+1:m_train+m_test+m_cv,3});         % Price column
-Theta_cv=zeros(n,1);
-k_cv=1;
+E_cv=(1/(2*m_cv))*sum((X_cv*Theta_train-Y_cv).^2);
+% Theta_cv=zeros(n,1);
+% k_cv=1;
+% 
+% E_cv(k_cv)=(1/(2*m_cv))*sum((X_cv*Theta_cv-Y_cv).^2);
 
-E_cv(k_cv)=(1/(2*m_cv))*sum((X_cv*Theta_cv-Y_cv).^2);
-
-R_cv=1;
-while R_cv==1                     % Gradient decent
-Alpha=Alpha*1;
-Theta_cv=Theta_cv-(Alpha/m_cv)*X_cv'*(X_cv*Theta_cv-Y_cv);
-k_cv=k_cv+1;
-E_cv(k_cv)=(1/(2*m_cv))*sum((X_cv*Theta_cv-Y_cv).^2);
-if E_cv(k_cv-1)-E_cv(k_cv)<0
-    break
-end 
-q=(E_cv(k_cv-1)-E_cv(k_cv))./E_cv(k_cv-1);
-if q <.000001;
-    R_cv=0;
-end
-end
+% R_cv=1;
+% while R_cv==1                     % Gradient decent
+% Alpha=Alpha*1;
+% Theta_cv=Theta_cv-(Alpha/m_cv)*X_cv'*(X_cv*Theta_cv-Y_cv);
+% k_cv=k_cv+1;
+% E_cv(k_cv)=(1/(2*m_cv))*sum((X_cv*Theta_cv-Y_cv).^2);
+% if E_cv(k_cv-1)-E_cv(k_cv)<0
+%     break
+% end 
+% q=(E_cv(k_cv-1)-E_cv(k_cv))./E_cv(k_cv-1);
+% if q <.000001;
+%     R_cv=0;
+% end
+% end
 
 
